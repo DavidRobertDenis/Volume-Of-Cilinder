2a83d568bb6916428b324c007cf81a19c4145a1a
DavidRobertDenis <110538874+DavidRobertDenis@users.noreply.github.com>
2024-01-03 14:02:56 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definirea variabilelor
+        double raza = 10;
+        double inaltime = 50;
+
+        // Calcularea volumului cilindrului
+        double volum = Math.PI * Math.Pow(raza, 2) * inaltime;
+
+        // Afișarea rezultatului
+        Console.WriteLine($"Volumul cilindrului cu raza {raza} și înălțimea {inaltime} este: {volum}");
+    }
+}
